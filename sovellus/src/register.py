3aaefdf7e7ec7316d40e2aa28fd443fab4da5010
Lauri Huldin <127834326+LHuldin@users.noreply.github.com>
2024-04-13 20:06:10 +0300

    update register.py

--- sovellus/src/register.py
+++ sovellus/src/register.py
@@ -2,7 +2,7 @@ class Register:
     def register(self):
         username = input("Valitse vähintään 4 merkkiä pitkä käyttäjätunnus: ")
         password = input("Valitse vähintään 4 merkkiä pitkä salasana: ")
-        if len(username) or len(password) < 4:
+        if len(username) < 4 or len(password) < 4:
             print("Liian lyhyt käyttäjätunnus tai salasana")
         else:
             with open("users.txt", "a") as file:
