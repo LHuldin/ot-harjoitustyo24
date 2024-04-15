b2d5f0e729fc39773c9612cbb5b3dafa824b5cbc
Lauri Huldin <127834326+LHuldin@users.noreply.github.com>
2024-04-15 21:07:05 +0300

    create tasks.py

---
+++ sovellus/src/tests/register_test.py
@@ -0,0 +1,9 @@
+import unittest
+from register import Register
+
+class TestRegister(unittest.TestCase):
+    def setUp(self):
+        print("Set up goes here")
+
+    def test_hello_world(self):
+        self.assertEqual("Hello world", "Hello world")
