5c8362cb2c9221d4fb200cdee0b39de94eb69979
Lauri Huldin <127834326+LHuldin@users.noreply.github.com>
2024-04-25 22:14:35 +0300

    add user_db_manager_test.py

---
+++ sovellus/src/tests/user_db_manager_test.py
@@ -0,0 +1,14 @@
+import unittest
+from user_db_manager import *
+
+class TestuUser_db_manager(unittest.TestCase):
+    def setUp(self):
+        print("Set up goes here")
+
+    def test_username_or_password_too_short(self):
+        test = add_user("afj", "345")
+        self.assertEqual(str(test), "False")
+
+    def test_username_and_password_length_ok(self):
+        test = add_user("afjf", "3456")
+        self.assertEqual(str(test), "True")
