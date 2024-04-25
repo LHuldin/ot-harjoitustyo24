f85c780fc9f2165ccb13644929871239e7e44bb1
Lauri Huldin <127834326+LHuldin@users.noreply.github.com>
2024-04-25 11:59:01 +0300

    delete older ui files

--- sovellus/src/hardware.py
+++
@@ -1,8 +0,0 @@
-class Hardware:
-    def __init__(self, type, model, manufacturer):
-        self.type = type
-        self.model = model
-        self.manufacturer = manufacturer
-
-    def __repr__(self):
-        return f"{self.type}: {self.model} ({self.manufacturer})"
